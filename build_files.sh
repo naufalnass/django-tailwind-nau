08d2110dc02e6767e79b1e2de2aaaf5a5cfba3b9
Naufal Nasrullah <naufalnasrullah19@gmail.com>
2022-12-06 23:53:20 +0700

    lastt

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
-echo " Build Start"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py colecstatic --noinput --clear
-echo " Build End"
+# build_files.sh
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
